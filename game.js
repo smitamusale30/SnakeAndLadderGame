ca433cd08149b3227143b4c0ed12f92672b51a98
smitamusale30 <smitamusale30@gmail.com>
2021-01-16 11:08:22 +0530

    add;getting the next move after dice are thrown

--- game.js
+++ game.js
@@ -1,7 +1,35 @@
 console.log("Welcome !!!");
 const START=0;
+let die=0,playerPostion=0;
+var option=["NoPlay","Ladder","Snake"];
+let move='';
 function dice()
 {
-    return Math.floor(Math.random()*6)+1;
+    die=Math.floor(Math.random()*6)+1;
+    console.log(die);
+    
+    move=option[Math.floor(Math.random()*option.length)];
+    console.log(move);
+
+    if(move=="NoPlay")
+    {
+        playerPostion=playerPostion;
+    }
+    else if(move=="Ladder")
+    {
+        playerPostion=playerPostion+die;
+    }
+    else 
+    {
+        if(playerPostion<=START)
+        {
+            playerPostion=START;
+        }
+        else
+        {
+            playerPostion=playerPostion-die;
+        }
+    }
+    return playerPostion;
 }
 console.log(dice());

57a490b0d0fc17130d2080799eec7ecdfbe9821b
smitamusale30 <smitamusale30@gmail.com>
2021-01-21 11:27:57 +0530

    update;gets a Ladder then plays again

--- game.js
+++ game.js
@@ -2,109 +2,88 @@ console.log("Welcome !!!");
 const START=0;
 let die=0,move='';
 var option=["NoPlay","Ladder","Snake"];
+let position=0;
+let player1=true,playerPostion1=0,diceThrown1=0;
 
-let player=true;playerPostion=0,diceThrown=0;
-
-let player1=false,playerPostion1=0,diceThrown1=0;
+let player2=false,playerPostion2=0,diceThrown2=0;
 
 function dice()
 {
     die=Math.floor(Math.random()*6)+1;
     console.log("Dice:  "+die);
-    diceThrown++;
+    //diceThrown1++;
 }
 function nextMove()
 {
     move=option[Math.floor(Math.random()*option.length)];
     console.log("The Move:  "+move);
 }
+function nextStep(postion)
+{
+    dice();
+    nextMove();
+    if(move=="NoPlay")
+    {
+        playerPostion=postion;
+    }
+    else if(move=="Ladder")
+    {
+        let temp=postion+die;
+        if(temp>100)
+        {
+            playerPostion=postion;
+        }
+        else
+        {
+            playerPostion=postion+die;
+            console.log("player position: "+playerPostion);
+            nextStep(playerPostion);
+        }
+    }
+    else 
+    {
+        let temp=postion-die;
+        if(temp<0)
+        {  
+            playerPostion=postion;
+        }
+        else
+        {
+            playerPostion=postion-die;
+        } 
+    }
+    return playerPostion;   
+}
 function main()
 {
-    while(playerPostion<10 && playerPostion1<10)
+    while(playerPostion1<100 && playerPostion2<100)
     {
-        if(player==true && player1==false)
+        if(player1==true && player2==false)
         {
-            console.log("player0");
-            dice();
-            nextMove();
-            if(move=="NoPlay")
-            {
-                playerPostion=playerPostion;
-            }
-            else if(move=="Ladder")
-            {
-                let temp=playerPostion+die;
-                if(temp>10)
-                {
-                    playerPostion=playerPostion;
-                }
-                else
-                {
-                    playerPostion=playerPostion+die;
-                }
-            }
-            else 
-            {
-                let temp=playerPostion-die;
-                if(temp<0)
-                {
-                    playerPostion=playerPostion;
-                }
-                else
-                {
-                    playerPostion=playerPostion-die;
-                } 
-            } 
-            console.log("Player at The position:  "+playerPostion);
-            player=false;
-            player1=true;
-        }
-        else
-        {
+            console.log("**********************************************");
             console.log("player1");
-            dice();
-            nextMove();
-
-            if(move=="NoPlay")
-            {
-                playerPostion1=playerPostion1;
-            }
-            else if(move=="Ladder")
-            {
-                let temp=playerPostion1+die;
-                if(temp>10)
-                {
-                    playerPostion1=playerPostion1;
-                }
-                else
-                {
-                    playerPostion1=playerPostion1+die;
-                }
-            }
-            else 
-            {
-                let temp=playerPostion1-die;
-                if(temp<0)
-                {
-                    playerPostion1=playerPostion1;
-                }
-                else
-                {
-                    playerPostion1=playerPostion1-die;
-                } 
-            } 
-            console.log("Player1 at The position:  "+playerPostion1);    
-            player=true;
+            playerPostion1=nextStep(playerPostion1);
+            console.log("Player1 at The position:  "+playerPostion1);
             player1=false;
-        }
-        if(playerPostion>playerPostion1)
-        {
-            console.log("Player 0 get win");
+            player2=true;
         }
         else
         {
-            console.log("Player 1 get win");
+            console.log("**********************************************");
+            console.log("player2");
+            playerPostion2=nextStep(playerPostion2);
+            console.log("Player2 at The position:  "+playerPostion2);
+            player1=true;
+            player2=false;
         }
+    }
+    if(playerPostion1>playerPostion2)
+    {
+        console.log("Player 1 get win");
+    }
+    else
+    {
+        console.log("Player 2 get win");
     }
 }
 main();
